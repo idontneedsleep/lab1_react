2588bda7cf94b84b28fa1ac54042d6ee21457201
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2022-12-26 21:14:32 +0200

    завдання 5 міста+

--- task4.js
+++ task4.js
@@ -4,3 +4,19 @@ const cities = [
     {id: 3, name: "New York"},
     {id: 4, name: "Las Vegas"}
 ]
+function Option(props) {
+    return <option id={props.id.id}>{props.name.name}</option>;
+}
+
+function Select() {
+    return <select>
+        <Option id={cities[0]} name={cities[0]}/>
+        <Option id={cities[1]} name={cities[1]}/>
+        <Option id={cities[2]} name={cities[2]}/>
+        <Option id={cities[3]} name={cities[3]}/>
+    </select>
+}
+
+ReactDOM.render((
+    <Select/>
+), document.getElementById('root'));
